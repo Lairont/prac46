a482514995fa5664d55562e341fd55c38fc0cafb
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-05 11:16:42 +0300

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    enum Days { Sat, Sun, Mon, Tue, Web, Thu, Fri }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days i;
+            for (i = Days.Sat; i <= Days.Fri; i++)
+            Console.WriteLine(i + " имеет значение " + (int)i);
+            Console.Read();
+        }
+    }
+}
