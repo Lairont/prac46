0867edb72a1bef568023b67902050c248bdfada0
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-05 11:16:23 +0300

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    enum Month
+    {
+        Январь, Февраль, Март, Апрель, Май, Июнь,
+        Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь
+    }
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Month i;
+            for (i = Month.Январь; i <= Month.Декабрь; i++)
+                Console.WriteLine($"{(int)i}\t{i}");
+            Console.Read();
+        }
+    }
+}
+
+
